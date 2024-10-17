8d13177c7ee2ff90508c4de4dbed4a06c42681cf
DESKTOP-INHLFUL\bk201 <linhtd.caet@gmail.com>
2024-10-17 21:29:26 +0700

    1st

---
+++ FireBaseRealTime/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Diagnostics;
+
+namespace FireBaseRealTime;
+
+public partial class ViewModel : ObservableObject
+{
+    private readonly FirebaseClient firebaseClient;
+
+    public ViewModel()
+    {
+        var firebaseUrl = "https://pjrealtimedatabase-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+        // Tạo FirebaseClient với URL database
+        firebaseClient = new FirebaseClient(firebaseUrl);
+
+        ListenForUserChanges();
+    }
+
+    [RelayCommand]
+    public async Task AddData()
+    {
+        var data = new { Name = "John Doe", Age = 30 };
+
+        // Ghi dữ liệu vào Firebase
+        var result = await firebaseClient
+            .Child("Users")     // Tạo hoặc truy cập node "Users"
+            .PostAsync(data);
+    }
+
+    public void ListenForUserChanges()
+    {
+        firebaseClient
+            .Child("Users")
+            .AsObservable<dynamic>()
+            .Subscribe(user =>
+            {
+                Debug.Print($"User {user.Key} changed: {user.Object}");
+            });
+    }
+
+}
